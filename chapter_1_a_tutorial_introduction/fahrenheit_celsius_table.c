f44397359a33f2c695d8ac9e44fccfe3dcf3d97e
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-01 11:59:51 -0300

    chapter 1 - fahreinheit-celsius with while implementation

---
+++ chapter_1_a_tutorial_introduction/fahrenheit_celsius_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table for
+for fahr = 0, 20, ..., 300 */
+void main()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
